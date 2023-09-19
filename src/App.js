359b5ff3cf94c912e81089a5355994342ad330ac
bassarcy <bassarcy@gmail.com>
2023-09-19 14:58:41 -0500

    Navbar component added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import logo from './logo.svg';
+import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Cleaning up the code</h1>
     </div>
   );
