95412669e26d25f2d32e4690d8c077f21f8025b6
bassarcy <bassarcy@gmail.com>
2023-09-20 12:16:38 -0500

    Links added to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,20 @@
+import { Link } from 'react-router-dom'
+
 function Navbar() {
   return(
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="gotosomewhere">Movie Browser</a>
+        <Link className="navbar-brand" to="/">Movie Browser</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="gotosomewhere">Home</a>
+            <Link className="nav-link active" aria-current="page" to="/about">About</Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="gotosomewhere">Link</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link disabled" href="gotosomewhere" aria-disabled="true">Disabled</a>
+            <Link className="nav-link disabled" href="gotosomewhere" aria-disabled="true">Coming Soon</Link>
           </li>
         </ul>
         <form className="d-flex" role="search">
