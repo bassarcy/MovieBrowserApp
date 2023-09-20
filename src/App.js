84243735a1e5207fd3322cad844ede505cc22a09
bassarcy <bassarcy@gmail.com>
2023-09-20 12:06:44 -0500

    Installed react-router-dom and set up Route for HomeView and AboutView

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css'
 import Navbar from './components/Navbar'
 import HomeView from './components/HomeView'
 import AboutView from './components/AboutView'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <HomeView />
-      <AboutView />
+      <Routes>
+        <Route path="/" element={<HomeView />} />
+        <Route path="/about" element={<AboutView />} />
+      </Routes>
     </div>
   );
 }
