960555c2a33389aae22265cb54e924c1c0ae8b65
bassarcy <bassarcy@gmail.com>
2023-09-22 11:50:23 -0500

    Created component to display search results

--- src/components/SearchView.js
+++ src/components/SearchView.js
@@ -1,15 +1,40 @@
 import Hero from './Hero'
 
-// tmdb API Key: 90d7018278ec88fc70a341903f492114
-// Example URL: https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&without_keywords=star%20wars
+const MovieCard = ({ movie }) => {
+    const posterUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+    return(
+        <div className="col-lg-3 col-md-3 col-2 my-4">
+            <div className="card">
+                <img src={posterUrl} className="card-img-top" alt={movie.original_title} />
+                <div className="card-body">
+                    <h5 className="card-title">{movie.original_title}</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="btn btn-primary">Show Details</a>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 const SearchView = ({ keyword, searchResults }) => {
     const title = `You are searching for ${keyword}`
-    console.log(searchResults, "are the search results")
+    
+    const resultsHtml = searchResults.map((obj, i) => {
+        return( 
+            <MovieCard movie={obj} key={i} />
+        )
+    })
+
     return (
         <div>
             <Hero text={title} />
-      
+            {resultsHtml &&
+                <div className="container">
+                    <div className="row">
+                        {resultsHtml}
+                    </div>
+                </div>
+            }
         </div>
     )
 }
