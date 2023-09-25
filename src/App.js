50e4a288847f268de510df3aed40555eb885e6ed
bassarcy <bassarcy@gmail.com>
2023-09-25 16:38:01 -0500

    Created a message to display and change to Hero text whenever search results cannot be found. Also fixed formatting on files.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,56 @@
-import './App.css'
-import { useState, useEffect } from 'react'
-import Navbar from './components/Navbar'
-import HomeView from './components/HomeView'
-import AboutView from './components/AboutView'
-import { Routes, Route } from 'react-router-dom'
-import SearchView from './components/SearchView'
-import MovieView from './components/MovieView'
+import "./App.css";
+import { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import HomeView from "./components/HomeView";
+import AboutView from "./components/AboutView";
+import { Routes, Route } from "react-router-dom";
+import SearchView from "./components/SearchView";
+import MovieView from "./components/MovieView";
 
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchText, setSearchText] = useState([""]);
 
-  const [searchResults, setSearchResults] = useState([])
-  const [searchText, setSearchText] = useState([''])
-
-useEffect(() => {
-    if(searchText) {
-      fetch(`https://api.themoviedb.org/3/search/movie?api_key=ab166ff82684910ae3565621aea04d62&language=en-US&query=${searchText}&page=1&include_adult=false`)
-        .then(response => response.json())
-        .then(data => {
-          setSearchResults(data.results)
-        })
+  useEffect(() => {
+    if (searchText) {
+      fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=ab166ff82684910ae3565621aea04d62&language=en-US&query=${searchText}&page=1&include_adult=false`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setSearchResults(data.results);
+        });
     }
-  }, [searchText])
- 
+  }, [searchText]);
 
   return (
     <div className="App">
-      <Navbar searchText={searchText} setSearchText={setSearchText} />
+      <Navbar
+        searchText={searchText}
+        setSearchText={setSearchText}
+      />
       <Routes>
-        <Route path="/" element={<HomeView />} />
-        <Route path="/about" element={<AboutView />} />
-        <Route path ="/search" element={<SearchView keyword={searchText} searchResults={searchResults} />} />
-        <Route path="/movie/:id" element={<MovieView />} />
+        <Route
+          path="/"
+          element={<HomeView />}
+        />
+        <Route
+          path="/about"
+          element={<AboutView />}
+        />
+        <Route
+          path="/search"
+          element={
+            <SearchView
+              keyword={searchText}
+              searchResults={searchResults}
+            />
+          }
+        />
+        <Route
+          path="/movie/:id"
+          element={<MovieView />}
+        />
       </Routes>
     </div>
   );
--- src/components/SearchView.js
+++ src/components/SearchView.js
@@ -1,44 +1,77 @@
-import Hero from './Hero'
-import {Link} from 'react-router-dom'
+import Hero from "./Hero";
+import { Link } from "react-router-dom";
 
 const MovieCard = ({ movie }) => {
-    const posterUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
-    const detailUrl = `/movie/${movie.id}`
-    return(
-        <div className="col-lg-3 col-md-3 col-2 my-4">
-            <div className="card">
-                <img src={posterUrl} className="card-img-top" alt={movie.original_title} />
-                <div className="card-body">
-                    <h5 className="card-title">{movie.original_title}</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <Link to={detailUrl} className="btn btn-primary">Show Details</Link>
-                </div>
-            </div>
+  const posterUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+  const detailUrl = `/movie/${movie.id}`;
+  return (
+    <div className="col-lg-3 col-md-3 col-2 my-4">
+      <div className="card">
+        <img
+          src={posterUrl}
+          className="card-img-top"
+          alt={movie.original_title}
+        />
+        <div className="card-body">
+          <h5 className="card-title">{movie.original_title}</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <Link
+            to={detailUrl}
+            className="btn btn-primary">
+            Show Details
+          </Link>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 const SearchView = ({ keyword, searchResults }) => {
-    const title = `You are searching for ${keyword}`
-    
-    const resultsHtml = searchResults.map((obj, i) => {
-        return( 
-            <MovieCard movie={obj} key={i} />
-        )
-    })
+  const title = `You are searching for ${keyword}`;
 
+  const resultsHtml = searchResults.map((obj, i) => {
     return (
+      <MovieCard
+        movie={obj}
+        key={i}
+      />
+    );
+  });
+
+  function renderResults() {
+    if (searchResults.at(0) !== undefined) {
+      return (
         <div>
-            <Hero text={title} />
-            {resultsHtml &&
-                <div className="container">
-                    <div className="row">
-                        {resultsHtml}
-                    </div>
-                </div>
-            }
+          <Hero text={title} />
+          {resultsHtml && (
+            <div className="container">
+              <div className="row">{resultsHtml}</div>
+            </div>
+          )}
         </div>
-    )
-}
+      );
+    } else {
+      return (
+        <div>
+          <Hero text="No results" />
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-8 offset-lg-2 my-5">
+                <p className="lead">
+                  I'm sorry, but we cannot find any results for {keyword}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  return renderResults();
+};
 
 export default SearchView;
