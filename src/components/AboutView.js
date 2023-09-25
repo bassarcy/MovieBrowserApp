4ade99e53b8bfb996938d0533155b6d1e20843dd
bassarcy <bassarcy@gmail.com>
2023-09-25 17:17:44 -0500

    Made search button in search bar functional and formatted a few more pages

--- src/components/AboutView.js
+++ src/components/AboutView.js
@@ -1,20 +1,23 @@
-import Hero from './Hero'
+import Hero from "./Hero";
 
 const AboutView = () => {
-  return(
+  return (
     <div>
-        <Hero text="About Us" />
-        <div className="container">
-            <div className="row">
-                <div className="col-lg-8 offset-lg-2 my-5">
-                    <p className="lead">
-                        About Us Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque numquam rerum maiores? Perferendis ad voluptatum quaerat, impedit blanditiis porro nam magni quis maxime alias repellendus, quisquam nisi assumenda. Quaerat, error?
-                    </p>
-                </div>
-            </div>
+      <Hero text="About Us" />
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8 offset-lg-2 my-5">
+            <p className="lead">
+              About Us Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Itaque numquam rerum maiores? Perferendis ad voluptatum quaerat,
+              impedit blanditiis porro nam magni quis maxime alias repellendus,
+              quisquam nisi assumenda. Quaerat, error?
+            </p>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default AboutView;
--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -1,4 +1,4 @@
-import Hero from './Hero'
+import Hero from "./Hero";
 
 const HomeView = () => {
   return (
@@ -8,13 +8,16 @@ const HomeView = () => {
         <div className="row">
           <div className="col-lg-8 offset-lg-2 my-5">
             <p className="lead">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae illum fugit minima explicabo dolorem! Cupiditate architecto illo expedita, quis ea eius sunt non distinctio quasi eligendi dolorem, ut excepturi rerum.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+              illum fugit minima explicabo dolorem! Cupiditate architecto illo
+              expedita, quis ea eius sunt non distinctio quasi eligendi dolorem,
+              ut excepturi rerum.
             </p>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default HomeView;
