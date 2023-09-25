3d803804ab93a77b6949f05c9a064bd4dea6c798
bassarcy <bassarcy@gmail.com>
2023-09-25 18:03:35 -0500

    added movie overview to cards on search page and found default image for movies without posters

--- src/components/SearchView.js
+++ src/components/SearchView.js
@@ -9,15 +9,14 @@ const MovieCard = ({ movie }) => {
       <div className="card">
         <img
           src={posterUrl}
-          className="card-img-top"
-          alt={movie.original_title}
+          className="card-img-top-custom"
+          alt=""
         />
         <div className="card-body">
-          <h5 className="card-title">{movie.original_title}</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
+          <h5 className="card-title card-original-title">
+            {movie.original_title}
+          </h5>
+          <p className="card-text card-overview">{movie.overview}</p>
           <Link
             to={detailUrl}
             className="btn btn-primary">
